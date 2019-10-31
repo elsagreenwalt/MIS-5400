cd9f203aa2ed8e77dce9bd2868939e5a2e513118
Elsa Greenwalt <elsa.greenwalt@earthsoft.com>
2019-10-30 20:26:19 -0600

    Refactored Data Acquisition. Added Data Persistence that is 2/3 of the way done.

--- Data Aquisition.py
+++ Data Aquisition.py
@@ -1,15 +1,24 @@
 import requests
 import pprint
+import time
+import json
 
+# Get the first page of data and wait ~one minute
 high_school_rankings_page_1 = requests.get(f'https://api.schooldigger.com/v1.2/rankings/schools/UT?level=High&perPage=50&appID=95713b9f&appKey=f18f10f88b5d17a6f0b986cfbbbdd446')
+time.sleep(75)
+
+# Get the second page of data and wait ~one minute
 high_school_rankings_page_2 = requests.get(f'https://api.schooldigger.com/v1.2/rankings/schools/UT?level=High&perPage=50&page=2&appID=95713b9f&appKey=f18f10f88b5d17a6f0b986cfbbbdd446')
+time.sleep(75)
+
+# Get the third page of data - don't need to wait a minute this time
 high_school_rankings_page_3 = requests.get(f'https://api.schooldigger.com/v1.2/rankings/schools/UT?level=High&perPage=50&page=3&appID=95713b9f&appKey=f18f10f88b5d17a6f0b986cfbbbdd446')
 
-
-printer = pprint.PrettyPrinter(indent=2)
-printer.pprint(high_school_rankings_page_1.json())
-printer.pprint(high_school_rankings_page_2.json())
-printer.pprint(high_school_rankings_page_3.json())
+# Place into list containing dictionary
+school_list = []
+school_list.append(json.loads(high_school_rankings_page_1.text)['schoolList'])
+school_list.append(json.loads(high_school_rankings_page_2.text)['schoolList'])
+school_list.append(json.loads(high_school_rankings_page_3.text)['schoolList'])
 
 
 #Data Description
@@ -17,3 +26,5 @@ printer.pprint(high_school_rankings_page_3.json())
 #   There are 128 rows of data (128 high schools). The data all seems valid and reliable, but there is one concern - we are only allowed 1 API call per minute and 20 per day,
 #   which is problematic because we need to make multiple API calls to get all pages of data. We need to figure out a way to make the calls one at a time
 #   and store the responses in one location.
+
+# Worked with Rim Dahhou.
---
+++ Data Persistence.py
@@ -0,0 +1,43 @@
+import pyodbc
+import csv
+import json
+
+# Set connection string for MIS5400 Database
+connection_string = 'Driver={ODBC Driver 17 for SQL Server};Server=tcp:mis5440eg.database.windows.net,1433;Database=MIS5400Project;Uid=elsagreenwalt;Pwd=Mis5400fa2019;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+
+# Return connection object
+conn = pyodbc.connect(connection_string,autocommit=True)
+
+# Create Cursor
+curs = conn.cursor()
+
+# Create our staging table
+curs.execute(
+    '''
+    create table MIS_5400_staging_table(
+    School_ID int primary key
+    ,School_Name string
+    ,Rank int
+    )
+
+    '''
+    )
+
+# Insert our data
+insert_query = 'insert into MIS_5400_staging_table (School_ID, School_Name, Ranking) values (?,?)' # Question marks as placeholders
+with open(r'/tmp/data/todayilearned.json') as data_file:
+    data = json.load(data_file)
+    data_list = data['data']['children']
+    rows_to_insert = []
+    for entry in data_list:
+        rows_to_insert.append(tuple([entry['data']['title'], entry['data']['domain']]))
+    curs.executemany(insert_query, rows_to_insert)
+
+
+# Commit
+conn.commit()
+# Close
+conn.close
+
+
+
