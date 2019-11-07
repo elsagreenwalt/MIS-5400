0342b1b4b620d669cd6f6d91a31c73bb0e64e198
Elsa Greenwalt <elsa.greenwalt@earthsoft.com>
2019-11-06 19:43:15 -0700

    Created APIs for getting school rankings and information.

---
+++ DataAccessibility.py
@@ -0,0 +1,108 @@
+from flask import Flask, g, render_template, abort, request
+import json
+import pyodbc
+
+###############
+# CREATE APIs #
+###############
+
+# Create connection
+connection_string = 'Driver={ODBC Driver 17 for SQL Server};Server=tcp:mis5440eg.database.windows.net,' \
+                    '1433;Database=MIS5400Project;Uid=elsagreenwalt;Pwd=Mis5400fa2019;Encrypt=yes' \
+                    ';TrustServerCertificate=no;Connection Timeout=30; '
+
+# Set up Flask
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Make sure we are connected to the database
+@app.before_request
+def before_request():
+    try:
+        g.sql_conn =  pyodbc.connect(connection_string, autocommit=True)
+    except Exception:
+        abort(500, "No database connection could be established.")
+
+# Tear down request
+@app.teardown_request
+def teardown_request(exception):
+    try:
+        g.sql_conn.close()
+    except AttributeError:
+        pass
+
+# GET all school data
+@app.route('/api/v1/schools', methods=['GET'])
+def get_school_data():
+    curs = g.sql_conn.cursor()
+    query = 'select * from MIS5400Project.dbo.School_Info'
+    curs.execute(query)
+
+    columns = [column[0] for column in curs.description]
+    data = []
+
+    for row in curs.fetchall():
+        data.append(dict(zip(columns, row)))
+    return json.dumps(data, indent=4, sort_keys=True, default=str)
+
+# GET all school rankings
+@app.route('/api/v1/rankings', methods=['GET'])
+def get_school_rankings():
+    curs = g.sql_conn.cursor()
+    query = 'select * from MIS5400Project.dbo.School_Rankings'
+    curs.execute(query)
+
+    columns = [column[0] for column in curs.description]
+    data = []
+
+    for row in curs.fetchall():
+        data.append(dict(zip(columns, row)))
+    return json.dumps(data, indent=4, sort_keys=True, default=str)
+
+
+# GET information for one school
+@app.route('/api/v1/schools/<string:school_id>', methods=['GET'])
+def get_single_school_data(school_id):
+    curs = g.sql_conn.cursor()
+    curs.execute("select * from MIS5400Project.dbo.School_Info where school_id = ?", school_id)
+
+    columns = [column[0] for column in curs.description]
+    data = []
+
+    for row in curs.fetchall():
+        data.append(dict(zip(columns, row)))
+
+    return json.dumps(data, indent=4, sort_keys=True, default=str)
+
+# GET ranking for one school
+@app.route('/api/v1/rankings/<string:school_id>', methods=['GET'])
+def get_single_school_ranking(school_id):
+    curs = g.sql_conn.cursor()
+    curs.execute("select * from MIS5400Project.dbo.School_Rankings where school_id = ?", school_id)
+
+    columns = [column[0] for column in curs.description]
+    data = []
+
+    for row in curs.fetchall():
+        data.append(dict(zip(columns, row)))
+
+    return json.dumps(data, indent=4, sort_keys=True, default=str)
+
+# GET all school rankings
+@app.route('/api/v1/all', methods=['GET'])
+def get_school_rankings_and_info():
+    curs = g.sql_conn.cursor()
+    query = 'SELECT i.School_Name, r.Rank, r.Avg_Standard_Score \
+            FROM MIS5400Project.dbo.School_Info as i \
+                JOIN MIS5400Project.dbo.School_Rankings as r on r.School_ID = i.School_ID'
+    curs.execute(query)
+
+    columns = [column[0] for column in curs.description]
+    data = []
+
+    for row in curs.fetchall():
+        data.append(dict(zip(columns, row)))
+    return json.dumps(data, indent=4, sort_keys=True, default=str)
+
+if __name__ == '__main__':
+    app.run()
--- DataPersistence.py
+++ DataPersistence.py
@@ -10,12 +10,12 @@ import time
 # Get the first page of data and wait ~one minute
 high_school_rankings_page_1 = requests.get(f'https://api.schooldigger.com/v1.2/rankings/schools/UT?level=High&perPage'
                                            f'=50&appID=95713b9f&appKey=f18f10f88b5d17a6f0b986cfbbbdd446')
-# time.sleep(75)
+time.sleep(75)
 
 # Get the second page of data and wait ~one minute
 high_school_rankings_page_2 = requests.get(f'https://api.schooldigger.com/v1.2/rankings/schools/UT?level=High&perPage'
                                            f'=50&page=2&appID=95713b9f&appKey=f18f10f88b5d17a6f0b986cfbbbdd446')
-# time.sleep(75)
+time.sleep(75)
 
 # Get the third page of data - don't need to wait a minute this time
 high_school_rankings_page_3 = requests.get(f'https://api.schooldigger.com/v1.2/rankings/schools/UT?level=High&perPage'
@@ -43,64 +43,125 @@ conn = pyodbc.connect(connection_string, autocommit=True)
 # Create Cursor
 curs = conn.cursor()
 
-# Create our staging table if it doesn't exist
-if curs.tables(table='MIS_5400_staging_table', tableType='TABLE').fetchone():
+# Create our school info table if it doesn't exist
+if curs.tables(table='School_Info', tableType='TABLE').fetchone():
     None
 else:
     curs.execute(
         '''
-        create table MIS_5400_staging_table(
+        create table School_Info(
         School_ID nvarchar(100) primary key
         ,School_Name varchar(100)
-        ,Rank int
-        ,Average_Standard_Score float
+        ,Street_Address varchar(100)
+        ,State varchar(100)
+        ,Zip varchar(20)
         )
 
         '''
     )
 
-# Set up insert query
-insert_query = 'insert into MIS_5400_staging_table (School_ID, School_Name, Rank, Average_Standard_Score) \
-                values (?,?,?,?)'  # Question marks as placeholders
+# Create our school rankings table if it doesn't exist
+if curs.tables(table='School_Rankings', tableType='TABLE').fetchone():
+    None
+else:
+    curs.execute(
+        '''
+        create table School_Rankings(
+        School_ID nvarchar(100) primary key
+        ,Rank int
+        ,Avg_Standard_Score varchar(100)
+        )
 
-# TODO figure out how to get this into one for loop
-# Insert the first page
-rows_to_insert = []
-for i in range(0, len(school_list[0])):
-    school_id = school_list[0][i]["schoolid"]
-    school_name = school_list[0][i]["schoolName"]
-    school_rank = school_list[0][i]["rankHistory"][0]["rank"]
-    school_score = school_list[0][i]["rankHistory"][0]["averageStandardScore"]
-    row = (school_id, school_name, school_rank, school_score)
-    rows_to_insert.append(row)
-curs.executemany(insert_query, rows_to_insert)
+        '''
+    )
 
-# Insert the second page
-rows_to_insert = []
-for i in range(0, len(school_list[1])):
-    school_id = school_list[1][i]["schoolid"]
-    school_name = school_list[1][i]["schoolName"]
-    school_rank = school_list[1][i]["rankHistory"][0]["rank"]
-    school_score = school_list[1][i]["rankHistory"][0]["averageStandardScore"]
-    row = (school_id, school_name, school_rank, school_score)
-    rows_to_insert.append(row)
-curs.executemany(insert_query, rows_to_insert)
+add_data = input("Yes/No: Would you like to add data to the database? ")
 
-# Insert the third page
-rows_to_insert = []
-for i in range(0, len(school_list[2])):
-    school_id = school_list[2][i]["schoolid"]
-    school_name = school_list[2][i]["schoolName"]
-    school_rank = school_list[2][i]["rankHistory"][0]["rank"]
-    school_score = school_list[2][i]["rankHistory"][0]["averageStandardScore"]
-    row = (school_id, school_name, school_rank, school_score)
-    rows_to_insert.append(row)
-curs.executemany(insert_query, rows_to_insert)
+if add_data == 'Yes':
+    # Set up insert query for School_Info table
+    insert_query = 'insert into School_Info (School_ID, School_Name, Street_Address, State, Zip) \
+                    values (?,?,?,?,?)'  # Question marks as placeholders
 
-# Commit
-conn.commit()
-# Close
-conn.close
+    # TODO figure out how to get this into one for loop
+    # Insert the first page
+    rows_to_insert = []
+    for i in range(0, len(school_list[0])):
+        school_id = school_list[0][i]["schoolid"]
+        school_name = school_list[0][i]["schoolName"]
+        street_address = school_list[0][i]["address"]["street"]
+        state = school_list[0][i]["address"]["state"]
+        zip = school_list[0][i]["address"]["zip"]
+        row = (school_id, school_name, street_address, state, zip)
+        rows_to_insert.append(row)
+    curs.executemany(insert_query, rows_to_insert)
+
+    # Insert the second page
+    rows_to_insert = []
+    for i in range(0, len(school_list[1])):
+        school_id = school_list[1][i]["schoolid"]
+        school_name = school_list[1][i]["schoolName"]
+        street_address = school_list[1][i]["address"]["street"]
+        state = school_list[1][i]["address"]["state"]
+        zip = school_list[1][i]["address"]["zip"]
+        row = (school_id, school_name, street_address, state, zip)
+        rows_to_insert.append(row)
+    curs.executemany(insert_query, rows_to_insert)
+
+    # Insert the third page
+    rows_to_insert = []
+    for i in range(0, len(school_list[2])):
+        school_id = school_list[2][i]["schoolid"]
+        school_name = school_list[2][i]["schoolName"]
+        street_address = school_list[2][i]["address"]["street"]
+        state = school_list[2][i]["address"]["state"]
+        zip = school_list[2][i]["address"]["zip"]
+        row = (school_id, school_name, street_address, state, zip)
+        rows_to_insert.append(row)
+    curs.executemany(insert_query, rows_to_insert)
+
+
+    # Set up insert query for School_Rankings table
+    insert_query = 'insert into School_Rankings (School_ID, Rank, Avg_Standard_Score) \
+                    values (?,?,?)'  # Question marks as placeholders
+
+    # TODO figure out how to get this into one for loop
+    # Insert the first page
+    rows_to_insert = []
+    for i in range(0, len(school_list[0])):
+        school_id = school_list[0][i]["schoolid"]
+        school_rank = school_list[0][i]["rankHistory"][0]["rank"]
+        school_score = school_list[0][i]["rankHistory"][0]["averageStandardScore"]
+        row = (school_id, school_rank, school_score)
+        rows_to_insert.append(row)
+    curs.executemany(insert_query, rows_to_insert)
+
+    # Insert the second page
+    rows_to_insert = []
+    for i in range(0, len(school_list[1])):
+        school_id = school_list[1][i]["schoolid"]
+        school_rank = school_list[1][i]["rankHistory"][0]["rank"]
+        school_score = school_list[1][i]["rankHistory"][0]["averageStandardScore"]
+        row = (school_id, school_rank, school_score)
+        rows_to_insert.append(row)
+    curs.executemany(insert_query, rows_to_insert)
+
+    # Insert the third page
+    rows_to_insert = []
+    for i in range(0, len(school_list[2])):
+        school_id = school_list[2][i]["schoolid"]
+        school_rank = school_list[2][i]["rankHistory"][0]["rank"]
+        school_score = school_list[2][i]["rankHistory"][0]["averageStandardScore"]
+        row = (school_id, school_rank, school_score)
+        rows_to_insert.append(row)
+    curs.executemany(insert_query, rows_to_insert)
+
+    # Commit
+    conn.commit()
+    # Close Connection
+    conn.close
+else:
+    None
+
 
 
 
